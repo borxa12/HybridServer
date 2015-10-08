748b148b8ff15c3ec8be98770eb489b641a67317
borxa12 <borxa12@gmail.com>
2015-10-08 03:56:26 +0200

    HybrirServer 1.0
    Primeras modificaciones de HTTPRequest

--- src/es/uvigo/esei/dai/hybridserver/http/HTTPRequest.java
+++ src/es/uvigo/esei/dai/hybridserver/http/HTTPRequest.java
@@ -1,36 +1,80 @@
 package es.uvigo.esei.dai.hybridserver.http;
 
+import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.Reader;
 import java.util.Map;
 
 public class HTTPRequest {
+	
+	private BufferedReader bf;
+	
 	public HTTPRequest(Reader reader) throws IOException, HTTPParseException {
+		bf = new BufferedReader(reader);
 	}
 
 	public HTTPRequestMethod getMethod() {
-		// TODO Auto-generated method stub
-		return null;
+		HTTPRequestMethod method = null;
+		try {
+			String line = bf.readLine();
+			int pos = line.lastIndexOf("\\s");
+			String metodo = line.substring(0,pos-1);
+			for(int i = 0; i < HTTPRequestMethod.values().length; i++) {
+				if(HTTPRequestMethod.values()[i].equals(metodo))
+					method = HTTPRequestMethod.values()[i];
+			}
+		} catch (IOException e) {
+			System.err.println(e.getMessage());
+		}
+		return method;
 	}
 
 	public String getResourceChain() {
-		// TODO Auto-generated method stub
-		return null;
+		String chain = null;
+		try {
+			String line = bf.readLine();
+			int pos = line.lastIndexOf("\\s");
+			String substring = line.substring(pos+1,line.length());
+			int pos2 = substring.lastIndexOf("\\s");
+			String substring2 = substring.substring(0,pos2-1);
+			int pos3 = substring2.indexOf('?');
+			chain = substring2.substring(0,pos3-1);
+		} catch (IOException e) {
+			System.err.println(e.getMessage());
+		}
+		return chain;
 	}
 
 	public String[] getResourcePath() {
-		// TODO Auto-generated method stub
-		return null;
+		String[] path = null;
+		path = this.getResourceChain().split("/");
+		return path;
 	}
 
 	public String getResourceName() {
-		// TODO Auto-generated method stub
-		return null;
+		String[] path = this.getResourcePath();
+		return path[path.length];
 	}
 
+	@SuppressWarnings("null")
 	public Map<String, String> getResourceParameters() {
-		// TODO Auto-generated method stub
-		return null;
+		Map<String,String> parameters = null;
+		try {
+			String line = bf.readLine();
+			int pos = line.indexOf('?');
+			String substring = line.substring(pos+1,line.length());
+			int pos2 = substring.indexOf("\\s");
+			String substring2 = substring.substring(0,pos2-1);
+			String[] aux = substring2.split("&");
+			for(int i = 0; i < aux.length; i++) {
+				String[] auxP;
+				auxP = aux[i].split("=");
+				parameters.put(auxP[0],auxP[1]);
+			}
+		} catch (IOException e) {
+			System.err.println(e.getMessage());
+		}
+		return parameters;
 	}
 
 	public String getHttpVersion() {

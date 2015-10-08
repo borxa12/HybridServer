f4cc6128444e3c72c0ddab109ba7656b4e53df1b
borxa12 <borxa12@gmail.com>
2015-10-08 04:01:14 +0200

    HybridServer 5.0
    Creación de la clase HTTPResponse. Supera todos los test excepto uno.

--- src/es/uvigo/esei/dai/hybridserver/HybridServer.java
+++ src/es/uvigo/esei/dai/hybridserver/HybridServer.java
@@ -1,32 +1,38 @@
 package es.uvigo.esei.dai.hybridserver;
 
+import java.io.IOException;
+import java.io.OutputStream;
+import java.net.ServerSocket;
+import java.net.Socket;
 import java.util.Map;
 import java.util.Properties;
 
+import es.uvigo.esei.dai.hybridserver.http.HTTPResponse;
+import es.uvigo.esei.dai.hybridserver.http.HTTPResponseStatus;
+
 public class HybridServer {
-   public HybridServer() {
-      // Constructor necesario para los tests de la primera semana
-   }
-   
-   public HybridServer(Map<String, String> pages) {
-      // Constructor necesario para los tests de la segunda semana
-   }
 
-   public HybridServer(Properties properties) {
-      // Constructor necesario para los tests de la tercera semana
-   }
+	public HybridServer() {
+		// Constructor necesario para los tests de la primera semana
+	}
 
-   public int getPort() {
-      // TODO Auto-generated method stub
-      return -1;
-   }
+	public HybridServer(Map<String, String> pages) {
+		// Constructor necesario para los tests de la segunda semana
+	}
 
-   public void start() {
-      // TODO Auto-generated method stub
-      
-   }
+	public HybridServer(Properties properties) {
+		// Constructor necesario para los tests de la tercera semana
+	}
 
-   public void stop() {
-      // TODO Auto-generated method stub
-   }
+	public int getPort() {
+		return -1;
+	}
+
+	public void start() {
+		
+	}
+
+	public void stop() {
+		
+	}
 }
--- src/es/uvigo/esei/dai/hybridserver/http/HTTPResponse.java
+++ src/es/uvigo/esei/dai/hybridserver/http/HTTPResponse.java
@@ -6,69 +6,100 @@ import java.io.Writer;
 import java.util.ArrayList;
 import java.util.Collection;
 import java.util.Iterator;
+import java.util.LinkedHashMap;
 import java.util.List;
 import java.util.Map;
+import java.util.Map.Entry;
 
 public class HTTPResponse {
-	
+
+	private String HTTPVersion;
+	private HTTPResponseStatus responseStatus;
+	private Map<String, String> headerParameters;
+	private String content;
+
 	public HTTPResponse() {
+		this.HTTPVersion = null;
+		this.responseStatus = null;
+		this.headerParameters = new LinkedHashMap<>();
+		this.content = null;
 	}
 
 	public HTTPResponseStatus getStatus() {
-		return this.getStatus();
+		return this.responseStatus;
 	}
 
 	public void setStatus(HTTPResponseStatus status) {
+		this.responseStatus = status;
 	}
 
 	public String getVersion() {
-		return null;
+		return this.HTTPVersion;
 	}
 
 	public void setVersion(String version) {
+		this.HTTPVersion = version;
 	}
 
 	public String getContent() {
-		return null;
+		return this.content;
 	}
 
 	public void setContent(String content) {
+		this.content = content;
 	}
 
 	public Map<String, String> getParameters() {
-		// TODO Auto-generated method stub
-		return null;
+		return this.headerParameters;
 	}
 
 	public String putParameter(String name, String value) {
-		// TODO Auto-generated method stub
-		return this.getParameters().put(name, value);
+		return this.headerParameters.put(name, value);
 	}
 
 	public boolean containsParameter(String name) {
-		// TODO Auto-generated method stub
-		return this.getParameters().containsKey(name);
+		return this.headerParameters.containsKey(name);
 	}
 
 	public String removeParameter(String name) {
-		// TODO Auto-generated method stub
-		return this.getParameters().remove(name);
+		return this.headerParameters.remove(name);
 	}
 
 	public void clearParameters() {
+		this.headerParameters.clear();
 	}
 
 	public List<String> listParameters() {
 		List<String> parameters = new ArrayList<>();
 		Collection<String> param = this.getParameters().values();
 		Iterator<String> it = param.iterator();
-		while(it.hasNext()) {
+		while (it.hasNext()) {
 			parameters.add(it.next().toString());
 		}
 		return parameters;
 	}
 
 	public void print(Writer writer) throws IOException {
+
+		writer.append(this.getVersion()).append(' ')
+			.append(String.valueOf(this.getStatus().getCode())).append(' ')
+			.append(this.getStatus().getStatus()).append("\r\n");
+		
+		if (this.headerParameters == null && this.content == null) { //Sin parametros && Sin contenido
+			writer.append("\r\n");
+		} else if (this.headerParameters != null) { // Con parametros
+			for (Entry<String, String> param : this.getParameters().entrySet()) {
+				writer.append(param.getKey()).append(": ").append(param.getValue()).append("\r\n");
+			}
+			writer.append("\r\n");
+
+		}
+		if (this.content != null) { // Con cotenido
+			writer.append("Content-Length: ").append(String.valueOf(this.getContent().length()))
+				.append("\r\n");
+			writer.append("\r\n");
+			writer.append(this.getContent());
+		}
 	}
 
 	@Override
@@ -80,6 +111,7 @@ public class HTTPResponse {
 		} catch (IOException e) {
 		}
 
+		System.out.println(writer.toString());
 		return writer.toString();
 	}
 }

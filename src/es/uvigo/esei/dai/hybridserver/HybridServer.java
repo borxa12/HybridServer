80831fdc1097d44728869345dee7291f0a5a9a2f
lchousal <lucia.chousal@gmail.com>
2015-10-12 19:57:41 +0200

    HybridServer 7.0

--- src/es/uvigo/esei/dai/hybridserver/HybridServer.java
+++ src/es/uvigo/esei/dai/hybridserver/HybridServer.java
@@ -7,13 +7,21 @@ import java.net.Socket;
 import java.util.Map;
 import java.util.Properties;
 
+import es.uvigo.esei.dai.hybridserver.http.HTTPRequest;
 import es.uvigo.esei.dai.hybridserver.http.HTTPResponse;
 import es.uvigo.esei.dai.hybridserver.http.HTTPResponseStatus;
 
 public class HybridServer {
+	private static final int SERVICE_PORT = 8888;
+    private Thread serverThread;
+    private boolean stop;
 
-	public HybridServer() {
+    private static final String WEB_PAGE = "<html><body><h1>Hola Mundo!!</h1></body></html>";
+
+    
+    public HybridServer() {
 		// Constructor necesario para los tests de la primera semana
+    	this.start();
 	}
 
 	public HybridServer(Map<String, String> pages) {
@@ -23,16 +31,65 @@ public class HybridServer {
 	public HybridServer(Properties properties) {
 		// Constructor necesario para los tests de la tercera semana
 	}
+    public int getPort() {
+        return SERVICE_PORT;
+    }
 
-	public int getPort() {
-		return -1;
-	}
+    public void start() {
+        this.serverThread = new Thread() {
+            @Override
+            public void run() {
+                try (final ServerSocket serverSocket = new ServerSocket(SERVICE_PORT)) {
+                    while (true) {
+                        try (Socket socket = serverSocket.accept()) {
+                            if (stop) {
+                                break;
+                            }
+                            // Responder al cliente
+                           /* OutputStream out = socket.getOutputStream();
+                            final int tam = WEB_PAGE.length();
+                            out.write("HTTP/1.1 200 OK\r\n".getBytes());
+                            out.write("Content-Type: text/html\r\n".getBytes());
+                            out.write(("Content-Length: " + tam + "\r\n").getBytes());
+                            out.write("\r\n".getBytes());
+                            out.write(WEB_PAGE.getBytes());
+                            out.flush();*/
+                            
+                            HTTPResponse response = new HTTPResponse();
+                            response.setVersion("HTTP/1.1");
+                            response.putParameter("Content-Type", "text/html");
+                            response.setContent(WEB_PAGE);
+                            
+                            
+                            
+                        }
+                    }
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+            }
+        };
 
-	public void start() {
-		
-	}
+        this.stop = false;
+        this.serverThread.start();
+    }
 
-	public void stop() {
-		
-	}
+    public void stop() {
+        this.stop = true;
+
+        try (Socket socket = new Socket("localhost", SERVICE_PORT)) {
+            // Esta conexión se hace, simplemente, para "despertar" el hilo servidor
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+
+        try {
+            this.serverThread.join();
+        } catch (InterruptedException e) {
+            throw new RuntimeException(e);
+        }
+
+        this.serverThread = null;
+    }
+
 }
--- src/es/uvigo/esei/dai/hybridserver/Launcher.java
+++ src/es/uvigo/esei/dai/hybridserver/Launcher.java
@@ -1,7 +1,22 @@
 package es.uvigo.esei.dai.hybridserver;
 
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.OutputStream;
+import java.io.Reader;
+import java.io.StringReader;
+import java.io.StringWriter;
+import java.net.Socket;
+import java.net.UnknownHostException;
+
+import es.uvigo.esei.dai.hybridserver.http.HTTPParseException;
+import es.uvigo.esei.dai.hybridserver.http.HTTPRequest;
+
 public class Launcher {
 	public static void main(String[] args) {
 		
+
+
 	}
 }
